7cd9c95a3ab6b1e8ad6db22fcce424ba70efd69b
mitilyas <mitilyas@mail.ru>
2019-12-05 20:38:25 +0400

    фикс регулярок

--- src/main/java/Parse/Parser.java
+++ src/main/java/Parse/Parser.java
@@ -59,8 +59,7 @@ public class Parser {
         while(string.indexOf(s, index)>=0){
                 int indexOfElement = string.indexOf(s, index);
                 string.insert(indexOfElement , ".");
-                string.insert(indexOfElement + 2, "?");
-                index = indexOfElement + 4;
+                index = indexOfElement + 2;
         }
         string.append("$");
     }
